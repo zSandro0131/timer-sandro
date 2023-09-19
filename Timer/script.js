12f7f08dfa71138eaf897ecaa835bc031ec0d101
zSandro0131 <135042150+zSandro0131@users.noreply.github.com>
2023-09-19 11:49:30 -0300

    Add files via upload
    Timer feito com JavaScript puro com um som de alarme quando o contador chegar em 0 segundos. 😊

---
+++ Timer/script.js
@@ -0,0 +1,44 @@
+const display = document.getElementById('timer');
+const startBtn = document.getElementById("start-btn");
+
+startBtn.addEventListener('click', () => {
+    const hours = document.getElementById("hours-input");
+    const minutes = document.getElementById("minutes-input");
+    const seconds = document.getElementById("seconds-input");
+
+    let duration = parseInt(hours.value * 60 * 60) + parseInt(minutes.value * 60) + parseInt(seconds.value);
+
+    timer(duration, display);
+});
+
+const timer = (duration, display) => {
+    let timer = duration;
+    let hours, minutes, seconds;
+
+    intervalId = setInterval(() => {
+        hours = Math.floor((timer / 60) / 60);
+        minutes = Math.floor((timer / 60) % 60);
+        seconds = Math.floor((timer % 60));
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+        timer -= 1;
+
+        if (timer < 0) {
+            const audio = document.getElementById("audio")
+            
+            clearInterval(intervalId)
+            display.innerHTML = "Time's Up!"
+            audio.play()
+        }
+
+    }, 1000);
+}
+
+
+
+    
